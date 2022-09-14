f3f3155b6aee07109fb49cd8ea32408e2d197eef
klozz <carlosj@klozz.dev>
2022-09-14 11:34:27 +0200

    allow neverallows on user builds
    Signed-off-by: klozz <carlosj@klozz.dev>
    Change-Id: I4fbcba243a588f2ef21d0b0046f5106f55f248a7

--- Android.mk
+++ Android.mk
@@ -81,6 +81,9 @@ endif
 
 NEVERALLOW_ARG :=
 ifeq ($(SELINUX_IGNORE_NEVERALLOWS),true)
+ifeq ($(TARGET_BUILD_VARIANT),user)
+$(warning SELINUX_IGNORE_NEVERALLOWS := true shouldn't be used in user builds)
+endif
 $(warning Be careful when using the SELINUX_IGNORE_NEVERALLOWS flag. \
           It does not work in user builds and using it will \
           not stop you from failing CTS.)
